85335fd4588f6b8b19aa847450a5b88d5f5e022a
Nasim Kabir <nasim.kabir@suffixit.com>
2024-05-06 17:08:32 +0600

    basic authorization server implementation done

---
+++ src/main/java/com/suffixit/oauth2_resource_server/config/CORSCustomizer.java
@@ -0,0 +1,52 @@
+package com.suffixit.oauth2_resource_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author nasimkabir
+ * ২৯/২/২৪
+ */
+@Component
+public class CORSCustomizer {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type",
+                "Accept",
+                "Origin",
+                "X-Requested-With"
+        ));
+        configuration.setExposedHeaders(List.of(
+                "Cache-Control",
+                "Content-Language",
+                "Content-Type",
+                "Expires",
+                "Last-Modified",
+                "Pragma"
+        ));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    public void corsCustomizer(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+    }
+}
--- src/main/java/com/suffixit/oauth2_resource_server/config/SecurityConfig.java
+++ src/main/java/com/suffixit/oauth2_resource_server/config/SecurityConfig.java
@@ -3,7 +3,17 @@ package com.suffixit.oauth2_resource_server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author nasimkabir
@@ -11,25 +21,64 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http.oauth2ResourceServer(
-//        c -> c.jwt(
-//            j -> j.jwkSetUri("http://localhost:8080/oauth2/jwks")
-//        )
-//    );
+    private static final String JWT_ROLE_NAME = "authorities";
+    private static final String ROLE_PREFIX = "";
 
-        http.oauth2ResourceServer(
+    private final CORSCustomizer corsCustomizer;
+
+    public SecurityConfig(CORSCustomizer corsCustomizer) {
+        this.corsCustomizer = corsCustomizer;
+    }
+
+
+/*    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.oauth2ResourceServer(
+        c -> c.jwt(
+            j -> j.jwkSetUri("http://localhost:8080/oauth2/jwks")
+        )
+    );
+
+       *//* http.oauth2ResourceServer(
                 c -> c.opaqueToken(
                         o -> o.introspectionUri("http://localhost:8080/oauth2/introspect")
                                 .introspectionClientCredentials("rs", "secret")
                 )
-        );
+        );*//*
 
         http.authorizeHttpRequests(
                 c -> c.anyRequest().authenticated()
         );
-
+        corsCustomizer.corsCustomizer(http);
         return http.build();
+    }*/
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        corsCustomizer.corsCustomizer(http);
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                /*.oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                        .decoder(JwtDecoders.fromIssuerLocation("http://localhost:9191"))
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        ))*/
+                /*.oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtSpec -> jwtSpec.jwkSetUri("http://localhost:9191/oauth2/jwks")))*/
+                .oauth2ResourceServer(oauth2-> oauth2.opaqueToken( o -> o.introspectionUri("http://localhost:9191/oauth2/introspect")
+                        .introspectionClientCredentials("client", "secret")))
+                .build();
+    }
+
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(JWT_ROLE_NAME);
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(ROLE_PREFIX);
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
